66d4802cf2684149b61eb890b7f579b3a59775ce
marynanesterenko <marynanesterenko95@gmail.com>
2022-11-27 17:19:57 -0500

    added two User Stories to String package

---
+++ src/UserStories_String/firstHalf.java
@@ -0,0 +1,25 @@
+package UserStories_String;
+
+/*
+UserStory_1:
+Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+Example:
+firstHalf("WooHoo") → "Woo"
+firstHalf("HelloThere") → "Hello"
+firstHalf("abcdef") → "abc"
+*/
+
+public class firstHalf {
+    public static void main(String[] args) {
+
+    }
+
+    // TO DO:
+    // uncomment lines 21 - 23, and write your method to solve this Task below:
+
+//    public String firstHalf(String str) {
+//
+//    }
+}
+
---
+++ src/UserStories_String/withoutEnd.java
@@ -0,0 +1,24 @@
+package UserStories_String;
+/*
+UserStory_2:
+Given a string, return a version without the first and last char, so "Hello" yields "ell".
+The string length will be at least 2.
+
+Example:
+withoutEnd("Hello") → "ell"
+withoutEnd("java") → "av"
+withoutEnd("coding") → "odin"
+*/
+
+public class withoutEnd {
+    public static void main(String[] args) {
+
+    }
+
+    // TO DO:
+    // uncomment lines 21 - 23, and write your method to solve this Task below:
+
+//    public String withoutEnd(String str) {
+//
+//    }
+}
