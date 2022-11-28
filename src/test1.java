78ca93b6a8e94dd324f842ea3f28496a4aa86d9e
livairyn <111402211+livairyn@users.noreply.github.com>
2022-11-28 13:16:08 -0500

    first

--- src/test1.java
+++ src/test1.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class test1 {
     public static void main(String[] args) {
+        String str = new String();
+
+
+        public String withoutEnd (String str) {
+
+            System.out.println(str.substring(0,str.length()-1));
+
+        }
 
     }
 
